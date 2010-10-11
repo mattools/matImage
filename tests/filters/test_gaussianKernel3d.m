91b2bc22ea1a4e49991ba9ffa335c1eefdba11e6
dlegland <david.legland@nantes.inra.fr>
2010-10-11 16:14:30 +0000

    filters: ass possibility to specify sigma as vector

---
+++ tests/filters/test_gaussianKernel3d.m
@@ -0,0 +1,41 @@
+function test_suite = test_gaussianKernel3d(varargin) %#ok<STOUT>
+%TEST_GAUSSIANKERNEL3D  One-line description here, please.
+%
+%   output = test_gaussianKernel3d(input)
+%
+%   Example
+%   test_gaussianKernel3d
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2010-10-11,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2010 INRA - Cepia Software Platform.
+
+
+initTestSuite;
+
+function test_scalarSize %#ok<*DEFNU>
+
+k = gaussianKernel3d(3, 2);
+
+assertEqual([3 3 3], size(k));
+assertAlmostEqual(1, sum(k(:)));
+
+function test_vectorSize
+
+k = gaussianKernel3d([7 7 5], 3);
+
+assertEqual([7 7 5], size(k));
+assertAlmostEqual(1, sum(k(:)));
+ 
+function test_vectorSigma
+
+k = gaussianKernel3d([7 7 5], [2 2 1]);
+
+assertEqual([7 7 5], size(k));
+assertAlmostEqual(1, sum(k(:)));
+ 

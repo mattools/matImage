012b580b09198c1ddb2c3039281827635c84ce3e
dlegland <david.legland@nantes.inra.fr>
2011-11-24 18:12:35 +0000

    imFilters: fix dependency bugs

--- tests/imFilters/test_imDistance.m
+++ tests/imFilters/test_imDistance.m
@@ -29,3 +29,10 @@ dim = [10 15];
 img = imDistance([10 15], [2 3;5 8;3 6], 'periodic');
 assertEqual(dim([2 1]), size(img));
 
+
+function test_removeEdge
+% Test with image dimension and points coords
+dim = [100 100];
+img = imDistance(dim, [10 10;10 90;90 10;90 90;50 50], 'remove');
+assertEqual(dim([2 1]), size(img));
+

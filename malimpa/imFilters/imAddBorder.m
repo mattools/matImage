5de0241e32087f4de444fc82a785cbe5d8ab383b
dlegland <david.legland@nantes.inra.fr>
2012-08-03 10:07:12 +0000

    imFilters/imAddBorder.m add support for color images

--- malimpa/imFilters/imAddBorder.m
+++ malimpa/imFilters/imAddBorder.m
@@ -2,18 +2,16 @@ function res = imAddBorder(img, pad, varargin)
 %IMADDBORDER  Add a border around a 2D or 3D image
 %
 %   IMG = imAddBorder(IMG, BORDER)
-%   adds BORDER pixels around the image. The size of new image is given by
+%   Adds BORDER pixels around the image. The size of new image is given by
 %   size(IMG)+2*BORDER. 
-%   The function works for 2D or 3D images.
+%   The function works for 2D or 3D images, grayscale or color.
 %
 %   IMG = imAddBorder(IMG, [BORDER1 BORDER2])
-%   or
 %   IMG = imAddBorder(IMG, [BORDER1 BORDER2 BORDER3])
 %   Specifies different borders for borders in first and second directions
 %   (and third direction for 3D images)
 %
 %   IMG = imAddBorder(IMG, [BORDER10 BORDER11 BORDER20 BORDER21])
-%   or
 %   IMG = imAddBorder(IMG, [BORDER10 BORDER11 BORDER20 BORDER21 BORDER30 BORDER31])
 %   Specifies different borders for borders in first and second directions,
 %   and in the beginning and at the end in each direction.
@@ -21,15 +19,22 @@ function res = imAddBorder(img, pad, varargin)
 %   IMG = imAddBorder(..., BACKGROUND)
 %   specifies the value of the background color.
 %
+%
 %   Example
-%   % Add 3 pixel in each side of a square
-%   img = imAddBorder(ones(4, 4), 3);
-%   imshow(img);
+%   % Add 20 pixel in each side of rice image
+%     img = imread('rice.png');
+%     img2 = imAddBorder(img, 20);
+%     imshow(img2);
 %
-%   % add borders with different sizes
-%   img = imAddBorder(ones(5, 5), [3 4 5 6]);
-%   imshow(img);
+%   % add white borders with different sizes
+%     img = imread('rice.png');
+%     img2 = imAddBorder(img, [10 20 30 40], 255);
+%     imshow(img2);
 %
+%   % add cyan border around a color image
+%     img = imread('peppers.png');
+%     img2 = imAddBorder(img, [30 20], [0 1 1]);
+%     imshow(img2);
 %
 %   See also
 %   padarray (does nearly similar job)
@@ -37,10 +42,14 @@ function res = imAddBorder(img, pad, varargin)
 %
 % ------
 % Author: David Legland
-% e-mail: david.legland@nantes.inra.fr
+% e-mail: david.legland@grignon.inra.fr
 % Created: 2007-08-24,    using Matlab 7.4.0.287 (R2007a)
 % Copyright 2007 INRA - BIA PV Nantes - MIAJ Jouy-en-Josas.
-% Licensed under the terms of the LGPL, see the file "license.txt"
+
+%   History
+%   2012.08.03 add support for color images
+
+%% Initialisations
 
 % extract background value
 background = 0;
@@ -50,17 +59,28 @@ end
 
 % size of input image
 dim = size(img);
+isColor = size(img, 3) == 3;
+isPlanar = length(dim) == 2 || (length(dim) == 3 && isColor);
 
-if length(dim)==2
+% in case of color image, ensure background is in the same range as image
+if max(background) <= 1 && max(img(:)) > 1
+    background = background * 255;
+end
+
+
+if isPlanar
+    %% Planar case 
+    
     % convert pad to have 4 values
-    if length(pad)==1
+    if length(pad) == 1
         pad = [pad pad pad pad];
     elseif length(pad)==2
         pad = [pad(1) pad(1) pad(2) pad(2)];
     end
 
     % compute new dimension
-    dim2 = dim + [sum(pad(1:2)) sum(pad(3:4))];
+    dim2 = dim;
+    dim2(1:2) = dim2(1:2) + [sum(pad(1:2)) sum(pad(3:4))];
 
     % create result image
     if islogical(img)
@@ -68,21 +88,36 @@ if length(dim)==2
     else
         res = ones(dim2, class(img));
     end
-    res(:) = background;
+    
+    % fill result with background color
+    if isColor && length(background) > 1
+        for c = 1:3
+            res(:,:,c) = background(c);
+        end
+    else
+        res(:) = background;
+    end
     
     % fillup result image with initial image
-    res((1:size(img, 1))+pad(1), (1:size(img, 2))+pad(3)) = img;
+    res((1:size(img, 1))+pad(1), (1:size(img, 2))+pad(3), :) = img;
     
-elseif length(dim)==3
+    
+elseif length(dim) - isColor == 3
+    %% Case of 3D image
+    
     % convert pad to have 6 values
-    if length(pad)==1
+    if length(pad) == 1
         pad = [pad pad pad pad pad pad];
-    elseif length(pad)==3
+    elseif length(pad) == 3
         pad = [pad(1) pad(1) pad(2) pad(2) pad(3) pad(3)];
     end
 
     % compute new dimension
-    dim2 = dim + [sum(pad(1:2)) sum(pad(3:4)) sum(pad(5:6))];
+    if isColor
+        dim2 = dim + [sum(pad(1:2)) sum(pad(3:4)) 0 sum(pad(5:6))];
+    else
+        dim2 = dim + [sum(pad(1:2)) sum(pad(3:4)) sum(pad(5:6))];
+    end
 
     % create result image
     if islogical(img)
@@ -90,12 +125,27 @@ elseif length(dim)==3
     else
         res = ones(dim2, class(img));
     end
-    res(:) = background;
+    
+    % fill result with background color
+    if isColor && length(background) > 1
+        for c = 1:3
+            res(:,:,c,:) = background(c);
+        end
+    else
+        res(:) = background;
+    end
     
     % fillup result image with initial image
-    res((1:size(img, 1))+pad(1),...
-        (1:size(img, 2))+pad(3), ...
-        (1:size(img, 3))+pad(5)) = img;
+    if isColor
+        res((1:size(img, 1)) + pad(1),...
+            (1:size(img, 2)) + pad(3), ...
+            :, ...
+            (1:size(img, 4)) + pad(5)) = img;
+    else
+        res((1:size(img, 1)) + pad(1),...
+            (1:size(img, 2)) + pad(3), ...
+            (1:size(img, 3)) + pad(5)) = img;
+    end
     
 else
     error('Image dimension not managed');
---
+++ tests/imFilters/test_imAddBorder.m
@@ -0,0 +1,58 @@
+function test_suite = test_imAddBorder(varargin) %#ok<STOUT>
+%TEST_IMADDBORDER  Test case for the file imAddBorder
+%
+%   Test case for the file imAddBorder
+
+%   Example
+%   test_imAddBorder
+%
+%   See also
+%
+%
+% ------
+% Author: David Legland
+% e-mail: david.legland@grignon.inra.fr
+% Created: 2012-08-03,    using Matlab 7.9.0.529 (R2009b)
+% Copyright 2012 INRA - Cepia Software Platform.
+
+initTestSuite;
+
+function test_Rice10 %#ok<*DEFNU>
+
+img = imread('rice.png');
+img2 = imAddBorder(img, 10);
+dim2 = size(img2);
+assertEqual([276 276], dim2);
+
+function test_Rice1020
+
+img = imread('rice.png');
+img2 = imAddBorder(img, [10 20]);
+dim2 = size(img2);
+assertEqual([276 296], dim2);
+
+function test_Rice10203040 
+
+img = imread('rice.png');
+img2 = imAddBorder(img, [10 20 30 40]);
+dim2 = size(img2);
+assertEqual([286 326], dim2);
+
+
+function test_Peppers
+
+img = imread('peppers.png');
+img2 = imAddBorder(img, 10);
+dim2 = size(img2);
+assertEqual([404 532 3], dim2);
+
+
+function test_Head_RGB
+
+load mri;
+img = cat(3, D, D, D);
+img2 = imAddBorder(img, 10);
+dim2 = size(img2);
+exp = size(img) + [2*10 2*10 0 2*10];
+assertEqual(exp, dim2);
+

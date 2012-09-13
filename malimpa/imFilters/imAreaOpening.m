44e5c475fd914e8dc6a69d7d9cd6598ea0c89901
dlegland <david.legland@nantes.inra.fr>
2012-09-13 09:32:56 +0000

    imFilters/imAreaOpening: better support of label images

--- malimpa/imFilters/imAreaOpening.m
+++ malimpa/imFilters/imAreaOpening.m
@@ -1,7 +1,16 @@
-function [bin inds] = imAreaOpening(lbl, areaMin, varargin)
+function [lbl inds] = imAreaOpening(img, areaMin, varargin)
 %IMAREAOPENING Remove all regions smaller than a given area
 %
-%   LBL2 = imAreaOpening(LBL)
+%   LBL2 = imAreaOpening(LBL, AREAMIN)
+%   Computes the area of each particle in label image LBL, and keep only
+%   the particles whose area is greater or equal to the given threshold.
+%   Returns another label image.
+%
+%   BIN2 = imAreaOpening(BIN, AREAMIN)
+%   BIN2 = imAreaOpening(BIN, AREAMIN, CONN)
+%   If BIN is a binary image, connected components are labelised first.
+%   The connectivity can be specified. The result is a binary image.
+%
 %
 %   Example
 %   % Remove rice grains smaller than a given size
@@ -20,20 +29,30 @@ function [bin inds] = imAreaOpening(lbl, areaMin, varargin)
 % Created: 2012-07-27,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2012 INRA - Cepia Software Platform.
 
+
 % in case of binary image, compute labels
-if islogical(lbl)
-    lbl = labelmatrix(bwconncomp(lbl, varargin{:}));
+if islogical(img)
+    lbl = labelmatrix(bwconncomp(img, varargin{:}));
+else
+    lbl = img;
 end
 
-% compute area of each label
+% initialisations
 nLabels = max(lbl(:));
 areas = zeros(nLabels, 1);
+
+% compute area of each label
 for i = 1:nLabels
     areas(i) = sum(sum(sum(lbl==i)));
 end
 
-% find index of largest regions
-inds = find(areas > areaMin);
+% find index of small regions
+inds = find(areas < areaMin);
 
-% keep as binary
-bin = ismember(lbl, inds);
+% remove small regions
+lbl(ismember(lbl, inds)) = 0;
+
+% keep same output type as input
+if islogical(img)
+    lbl = lbl > 0;
+end

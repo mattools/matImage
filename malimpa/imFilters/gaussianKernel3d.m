17b849d03ae2e3dc7146652e132d61c7b31d5040
dlegland <david.legland@nantes.inra.fr>
2013-02-25 13:21:12 +0000

    imFilters/gaussianKernel3d.m: add support for 3 outputs

--- malimpa/imFilters/gaussianKernel3d.m
+++ malimpa/imFilters/gaussianKernel3d.m
@@ -1,19 +1,37 @@
-function h = gaussianKernel3d(size, sigma)
+function varargout = gaussianKernel3d(size, sigma, varargin)
 %GAUSSIANKERNEL3D Create a 3D gaussian kernel for image filtering
 %
 %   K = gaussianKernel3d(SIZE, SIGMA)
 %   SIZE is either a scalar or a 1-by-3 row vector. SIGMA is the standard
-%   deviation of gaussian function.
+%   deviation of gaussian function. SIZE must contain odd values. SIZE and
+%   SIGMA vectors are given in XYZ order (not the matlab indexing order).
+%   The kernel is normalised such that the sum of its values equals 1.
 %
-%   Example
-%   K = gaussianKernel3d(3, 2);
-%   K = gaussianKernel3d([7 7 5], 3);
+%   [KX KY KZ] = gaussianKernel3d(SIZE, SIGMA)
+%   Returns the elementary linear kernels for each direction of the 3D
+%   image. This can be used for decompose the 3D gaussien filtering. Each
+%   kernel is normalised such that the sum of its values equals 1. Note
+%   that the function 'imGaussianFilter' encapsulates the whoke processing. 
 %
-%   Note: it is faster to use the function 'imGaussFilter', that uses
+%   Examples
+%     % Basic 3D kernel:
+%     K0 = gaussianKernel3d(3, 2);
+%
+%     % Larger kernel with non equal sizes:
+%     K = gaussianKernel3d([7 7 5], 3);
+%   
+%     % Create the same kernel using separate linear kernels:
+%     [KX KY KZ] = gaussianKernel3d([7 7 5], 3);
+%     K2 = bsxfun(@times, KY * KX, KZ);   % create 3D kernel
+%     sum(abs(K(:) - K2(:)) > 1e-15)      % number of significant differences
+%     ans =
+%         0
+%
+%   Note: it is faster to use the function 'imGaussianFilter', that uses
 %   separability of the gaussian kernel.
 %   
 %   See also
-%   imGaussFilter, fspecial, imfilter
+%   imGaussianFilter, fspecial, imfilter
 %
 % ------
 % Author: David Legland
@@ -21,34 +39,69 @@ function h = gaussianKernel3d(size, sigma)
 % Created: 2010-06-29,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2010 INRA - Cepia Software Platform.
 
+%   HISTORY
+%   2013-02-25 add possibility to get separated kernels
 
-% ensure size is a vector
-if length(size)==1
+
+%% Process input
+
+% ensure size is a vector of length 3
+if length(size) == 1
     size = [size size size];
 end
 
-% ensure sigma is a vector
-if length(sigma)==1
+% ensure sigma is a vector of length 3
+if length(sigma) == 1
     sigma = [sigma sigma sigma];
 end
 
+
+%% Pre-processing
+
+% create basis along each direction
+nx = round((size(1)-1) / 2);
+ny = round((size(2)-1) / 2);
+nz = round((size(3)-1) / 2);
+
 % compute reference for each axis
-nx = round((size(1)-1)/2);
-ny = round((size(2)-1)/2);
-nz = round((size(3)-1)/2);
+lx = -nx:nx;
+ly = -ny:ny;
+lz = -nz:nz;
 
-% create 3D grid
-[x y z] = meshgrid(-nx:nx, -ny:ny, -nz:nz);
+% normalise kernel width
+sigma2 = sigma .* sigma;
+sx2 = sigma2(1) * .5;
+sy2 = sigma2(2) * .5;
+sz2 = sigma2(3) * .5;
 
-% compute gaussian function for each point of the grid
-sigma2 = sigma.*sigma;
 
-%h = exp(-(x.^2 + y.^2 + z.^2)/(2*sigma2));
-sx2 = sigma2(1);
-sy2 = sigma2(2);
-sz2 = sigma2(3);
-h = exp(-(x.^2 / 2/sx2)) .* exp(-(y.^2 / 2/sy2)) .* exp(-(z.^2 / 2/sz2));
+%% Compute kernel(s)
 
-% normalize intensity
-h = h/(sum(h(:)));
+if nargout == 1
+    % Computes a single 3D kernel
+    
+    % create 3D grid
+    [x y z] = meshgrid(lx, ly, lz);
+    
+    % compute gaussian function for each point of the grid
+    h = exp(-(x.^2 / sx2)) .* exp(-(y.^2 / sy2)) .* exp(-(z.^2 / sz2));
+    
+    % normalize intensity
+    h = h / (sum(h(:)));
 
+    varargout = {h};
+    
+else
+    % computes kernel for each direction, for applying separability
+    hx = reshape(exp(-(lx.^2 / sx2)), [1 length(lx) 1]);
+    hy = reshape(exp(-(ly.^2 / sy2)), [length(ly) 1 1]);
+    hz = reshape(exp(-(lz.^2 / sz2)), [1 1 length(lz)]);
+
+    % normalize intensities
+    hx = hx / sum(hx(:));
+    hy = hy / sum(hy(:));
+    hz = hz / sum(hz(:));
+    
+    varargout = {hx, hy, hz};
+
+end

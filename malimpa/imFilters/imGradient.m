aa1a0c75061650feb27d6397d5aeec21f9fa5890
dlegland <david.legland@nantes.inra.fr>
2013-05-20 20:07:40 +0000

    imFilters/imGradient.m: add support for variable gradient width

--- malimpa/imFilters/imGradient.m
+++ malimpa/imFilters/imGradient.m
@@ -15,6 +15,10 @@ function varargout = imGradient(img, varargin)
 %   - hypot(dx, dy) for 2D images
 %   - hypot(hypot(dx, dy), dz) for 3D images
 %
+%   GRAD = imGradient(IMG, SIGMA);
+%   Specifies the width of the kernel (2D only). The size of the kernel is
+%   determined automatically from the SIGMA.
+%
 %   GRAD = imGradient(IMG, 'filter', FILTER);
 %   Specifies the filter used for computing gradient in the X direction.
 %   Note that fspecial('sobel') or fspecial('prewitt') return unnormalized
@@ -63,6 +67,7 @@ function varargout = imGradient(img, varargin)
 % 2010-03-03 use convolution by default
 % 2010-03-05 return result as double, normalize default filter
 % 2010-12-06 use 3D kernel by default for 3D images
+% 2013-05-20 add support for variable kernel width (2D only)
 
 
 %% Parse input arguments
@@ -73,16 +78,40 @@ dim = size(img);
 % number of dimension of image (do not manage color images)
 nd = length(dim);
 
+% check if the width of the kernel is specified
+sigma = 0;
+if ~isempty(varargin) 
+    var1 = varargin{1};
+    if isnumeric(var1) && isscalar(var1)
+        sigma = var1;
+        varargin(1) = [];
+    end
+end
+
+
 % default filter for gradient: normalised sobel
 if nd <= 2
-    sx = fspecial('sobel')'/8;
+    if sigma == 0
+        % Default 2D case: normalised sobel matrix
+        sx = fspecial('sobel')'/8;
+    else
+        lx = (-3*sigma):(3*sigma);
+        sy = exp(-((lx / sigma) .^2) * .5);
+        sx = -(lx / sigma) .* sy;
+        sx = sy' * sx;
+        sx = sx / sum(sx(sx > 0));
+    end
     
 elseif nd == 3
-    % use normalisation of 2 sobel matrices
-    base = [1 2 1]'*[1 2 1];
-    base = base/sum(base(:))/2;
-    sx = permute(cat(3, base, zeros(3, 3), -base), [2 3 1]);
-
+    if sigma == 0
+        % Default 3D case: normalisation of 2 classical sobel matrices
+        base = [1 2 1]' * [1 2 1];
+        base = base / sum(base(:))/2;
+        sx = permute(cat(3, base, zeros(3, 3), -base), [2 3 1]);
+    else
+        error('Kernel size not supported for 3D images');
+    end
+    
 else
     error('Input image must have 2 or 3 dimensions');
 end

08f2706db9cd12f8a7b9d499a3554f2a26ede6e3
dlegland <david.legland@nantes.inra.fr>
2015-08-25 15:47:15 +0200

    add imKirschFilter.m and imRobinsonFilter.m

---
+++ matImage/imFilters/imKirschFilter.m
@@ -0,0 +1,38 @@
+function [res, index] = imKirschFilter(img)
+%IMKIRSCHFILTER Extract image edges using Kirsch directional filters
+%
+%   output = imKirschFilter(input)
+%
+%   Example
+%     img = imread('rice.png');
+%     imgf = imKirschFilter(img);
+%     figure; imshow(imgf);
+%
+%   See also
+%     imRobinsonFilter, imGradient
+ 
+% ------
+% Author: David Legland
+% e-mail: david.legland@nantes.inra.fr
+% Created: 2015-05-06,    using Matlab 8.4.0.150421 (R2014b)
+% Copyright 2015 INRA - Cepia Software Platform.
+
+g1 = [ 5  5  5 ; -3 0 -3 ; -3 -3 -3];
+g2 = [ 5  5 -3 ;  5 0 -3 ; -3 -3 -3];
+g3 = [ 5 -3 -3 ;  5 0 -3 ;  5 -3 -3];
+g4 = [-3 -3 -3 ;  5 0 -3 ;  5  5 -3];
+g5 = [-3 -3 -3 ; -3 0 -3 ;  5  5  5];
+g6 = [-3 -3 -3 ; -3 0  5 ; -3  5  5];
+g7 = [-3 -3  5 ; -3 0  5 ; -3 -3  5];
+g8 = [-3  5  5 ; -3 0  5 ; -3 -3 -3];
+
+kernels = {g1, g2, g3, g4, g5, g6, g7, g8};
+
+res = zeros(size(img), class(img)); %#ok<ZEROLIKE>
+index = zeros(size(img), 'int8');
+for i = 1:8
+    imgf = imfilter(img, kernels{i}, 'replicate');
+    inds = imgf >= img;
+    res(inds) = imgf(inds);
+    index(inds) = i;
+end
---
+++ matImage/imFilters/imRobinsonFilter.m
@@ -0,0 +1,38 @@
+function [res, index] = imRobinsonFilter(img)
+%IMROBINSONFILTER Extract image edges using Robinson directional filters
+%
+%   output = imRobinsonFilter(input)
+%
+%   Example
+%     img = imread('rice.png');
+%     imgf = imRobinsonFilter(img);
+%     figure; imshow(imgf);
+%
+%   See also
+%     imKirschFilter, imGradient
+ 
+% ------
+% Author: David Legland
+% e-mail: david.legland@nantes.inra.fr
+% Created: 2015-05-06,    using Matlab 8.4.0.150421 (R2014b)
+% Copyright 2015 INRA - Cepia Software Platform.
+
+g1 = [ 1  2  1 ;  0 0  0 ; -1 -2 -1]; % N
+g2 = [ 2  1  0 ;  1 0 -1 ;  0 -1 -2]; % NE
+g3 = [ 1  0 -1 ;  2 0 -2 ;  1  0 -1]; % E
+g4 = [ 0 -1 -2 ;  1 0 -1 ;  2  1  0]; % SE
+g5 = [-1 -2 -1 ;  0 0  0 ;  1  2  1]; % S
+g6 = [-2 -1  0 ; -1 0  1 ;  0  1  2]; % SW
+g7 = [-1  0  1 ; -2 0  2 ; -1  0  1]; % W
+g8 = [ 0  1  2 ; -1 0  1 ; -2 -1  0]; % NW
+
+kernels = {g1, g2, g3, g4, g5, g6, g7, g8};
+
+res = zeros(size(img), class(img)); %#ok<ZEROLIKE>
+index = zeros(size(img), 'int8');
+for i = 1:8
+    imgf = imfilter(img, kernels{i}, 'replicate');
+    inds = imgf >= img;
+    res(inds) = imgf(inds);
+    index(inds) = i;
+end

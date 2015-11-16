fd5fa1dc5719797fc2108ea2a3598df7fc3bfb00
dlegland <david.legland@nantes.inra.fr>
2015-11-16 18:13:37 +0100

    imGranulometry: add Contents.m

---
+++ matImage/imGranulometry/Contents.m
@@ -0,0 +1,26 @@
+% IMGRANULOMETRY
+%
+%   Provides functions to compute granulometry curves on grayscale images
+%   using mathematical morphology operations.
+%
+%
+% Files
+%   granuloMeanSize   - Compute geometric mean of granulometric curve
+%   imGranulo         - Compute granulometry curve of a given image
+%   imGranuloByRegion - Granulometry curve for each region of label image
+%
+%
+% References
+% * Soille P, 2003, "Morphological Image Analysis", Springer.
+% * Devaux MF and Legland D, 2014, "Grey level granulometry for
+%   histological image analysis of plant tissues". Formatex Research
+%   Center, pp. 681-688.
+%   http://www.formatex.info/microscopy6/book/681-688.pdf
+%
+%
+% -----
+% Author: David Legland
+% e-mail: david.legland@nantes.inra.fr
+% Copyright INRA - Cepia Software Platform.
+% http://github.com/dlegland/matImage
+% http://www.pfl-cepia.inra.fr/index.php?page=imael

913a7f9dda2b4545d6e1a599c14d325576c0374d
dlegland <david.legland@nantes.inra.fr>
2016-06-06 13:50:07 +0200

    imMeasures/imJointHistogram: add support for ROI

--- matImage/imMeasures/imJointHistogram.m
+++ matImage/imMeasures/imJointHistogram.m
@@ -34,9 +34,10 @@ function res = imJointHistogram(img1, img2, varargin)
 %   See also
 %   imhist, imHistogram, imJointEntropy
 %
+
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@nantes.inra.fr
 % Created: 2009-12-09,    using Matlab 7.9.0.529 (R2009b)
 % Copyright 2010 INRA - Cepia Software Platform.
 
@@ -49,6 +50,9 @@ function res = imJointHistogram(img1, img2, varargin)
 if sum(size(img1) ~= size(img2)) > 0
     error('Inputs must have same size');
 end
+
+% default is no ROI (use the whole pixels in images)
+roi = ones(size(img1));
 
 % default number of intervals for computing values
 q1 = 256;
@@ -68,6 +72,12 @@ end
 
 % init to empty array
 vals1 = [];
+
+% check if a ROI is specified
+if ~isempty(varargin) && sum(size(img1) ~= size(varargin{1})) == 0
+    roi = varargin{1};
+    varargin(1) = [];
+end
 
 % extract user-specified number of bins, or bin values
 if length(varargin)==1
@@ -102,21 +112,24 @@ end
 % initialize result array
 res = zeros(length(vals1), length(vals2));
 
+% linear indices of pixels to consider
+inds = find(roi);
+
 if q1==256 && q2==256 && maxVal1==255 && maxVal2 == 255
     % Standard case: iterate over all pixels, use true value
-    for i=1:numel(img1)
+    for i = 1:length(inds)
         % get each value, and convert to index
-        v1 = img1(i) + 1;
-        v2 = img2(i) + 1;
+        v1 = img1(inds(i)) + 1;
+        v2 = img2(inds(i)) + 1;
         
         % increment corresponding histogram
         res(v1, v2) = res(v1, v2)+1;
     end
 else
-    for i=1:numel(img1)
+    for i = 1:length(inds)
         % get each value, and convert to index
-        v1 = find(img1(i)>=vals1, 1, 'last');
-        v2 = find(img2(i)>=vals2, 1, 'last');
+        v1 = find(img1(inds(i))>=vals1, 1, 'last');
+        v2 = find(img2(inds(i))>=vals2, 1, 'last');
 
         % increment corresponding histogram
         res(v1, v2) = res(v1, v2)+1;

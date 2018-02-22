15fb168059a3d43ea13db5c5d472a5f07d510df3
dlegland <david.legland@inra.fr>
2018-02-22 17:44:48 +0100

    imFilters/imDistanceMap.m: now supports contiguous labels, and number of weights equal to 3

--- matImage/imFilters/imDistanceMap.m
+++ matImage/imFilters/imDistanceMap.m
@@ -18,16 +18,18 @@ function dist = imDistanceMap(img, varargin)
 %   distance between two orthonal pixels, and WEIGHTS(2) corresponding to
 %   the distance between two diagonal pixels.
 %   Possible choices
-%   WEIGHTS = [1 sqrt(2)]   -> quasi-euclidean distance (default)
+%   WEIGHTS = [1 sqrt(2)]   -> quasi-euclidean distance
 %   WEIGHTS = [1 Inf]       -> "Manhattan" or "cityblock" distance
 %   WEIGHTS = [1 1]         -> "Chessboard" distance
 %   WEIGHTS = [3 4]         -> Borgerfors' weights
 %   WEIGHTS = [5 7]         -> close approximation of sqrt(2)
+%   WEIGHTS = [5 7 11]      -> Uses an additional weight for chess-knight
+%                              shifts around each pixel( default)
 %
 %   Note: when specifying weights, the result has the same class/data type
 %   than the array of weights. It is possible to balance between speed and
 %   memory usage:
-%   - if weights are double (the default), the memory usage is high, but
+%   - if weights are double (the default), the memory usage is larger, but
 %       the result can be given in pixel units 
 %   - if weights are integer (for Borgefors weights, for example), the
 %       memory usage is reduced, but representation limit of datatype can
@@ -37,6 +39,10 @@ function dist = imDistanceMap(img, varargin)
 %       around 255/3 = 85 pixels. Using 'int16'  seems to be a good
 %       tradeoff, the maximal distance with [3 4] weights is around 11000
 %       pixels.
+%
+%   RES = imDistanceMap(..., 'normalize', TF);
+%   If TF is true, normalizes the resulting map by the first weight.
+%   Default is true.
 %
 %   RES = imDistanceMap(..., 'verbose', true);
 %   Displays info on iterations.
@@ -80,35 +86,33 @@ function dist = imDistanceMap(img, varargin)
 %   HISTORY
 %   2010-08-25 fix memory allocation for large images, add vebosity option
 %   2012-08-20 adapt imChamferDistance to create imDistanceMap
+%   2018-02-22 add management of contiguous labels, and of three weights
+
 
 %% Process input arguments
 
 % default weights for orthogonal or diagonal
-w1 = 1;
-w2 = sqrt(2);
+weights = [5 7 11];
+
+normalize = true;
 
 % extract user-specified weights
 if ~isempty(varargin)
-    var = varargin{1};
+    weights = varargin{1};
     varargin(1) = [];
-    w1 = var(1);
-    w2 = var(2);
-    
-    % small check up to avoid degenerate cases
-    if w2 < w1
-        w2 = 2 * w1;
-    end
 end
-
 
 % extract verbosity option
 verbose = false;
-if length(varargin)>1
+if length(varargin) > 1
     varName = varargin{1};
     if ~ischar(varName)
-        error('unknown option');
+        error('Require options as name-value pairs');
     end
-    if strcmpi(varName, 'verbose')
+    
+    if strcmpi(varName, 'normalize')
+        normalize = varargin{2};
+    elseif strcmpi(varName, 'verbose')
         verbose = varargin{2};
     else
         error(['unknown option: ' varName]);
@@ -119,17 +123,33 @@ end
 %% Initialisations
 
 % determines type of output from type of weights
-outputType = class(w1);
+outputType = class(weights);
+
+% small check up to avoid degenerate cases
+w1 = weights(1);
+w2 = weights(2);
+if w2 < w1
+    w2 = 2 * w1;
+end
 
 % shifts in directions i and j for (1) forward and (2) backward iterations
-di1 = [-1 -1 -1 0];
-dj1 = [-1 0 1 -1];
-di2 = [+1 +1 +1 0];
-dj2 = [-1 0 1 +1];
-ws =  [w2 w1 w2 w1];
-
-% binarisation of img image
-img = img > 0;
+if length(weights) == 2
+    nShifts = 4;
+    di1 = [-1 -1 -1  0];
+    dj1 = [-1  0  1 -1];
+    di2 = [+1 +1 +1  0];
+    dj2 = [-1  0  1 +1];
+    ws =  [w2 w1 w2 w1];
+    
+elseif length(weights) == 3
+    nShifts = 8;
+    w3 = weights(3);
+    di1 = [-2 -2 -1 -1 -1 -1 -1  0];
+    dj1 = [-1 +1 -2 -1  0  1 +2 -1];
+    di2 = [+2 +2 +1 +1 +1 +1 +1  0];
+    dj2 = [-1 +1 +2 +1  0 -1 -2 +1];
+    ws =  [w3 w3 w3 w2 w1 w2 w3 w1];
+end
 
 % allocate memory for result
 dist = ones(size(img), outputType);
@@ -149,52 +169,43 @@ dist(~img) = 0;
 %% Forward iteration
 
 if verbose
-    disp(sprintf('Forward iteration %d', nIter)); %#ok<DSPS>
+    disp('Forward iteration %d');
 end
 
-% Process first line
-for j = 2:D2
-    if img(1, j)
-        dist(1,j) = min(dist(1,j), dist(1, j-1)+w1);     % left pixel
-    end
-end
-
-% iteration on lines
-for i = 2:D1
-    % special processing for first pixel of the line
-    if img(i, 1)
-        newVal = dist(i, 1);
-        newVal = min(newVal, dist(i-1, 1)+w1);     % top pixel
-        newVal = min(newVal, dist(i-1, 2)+w2);     % top-left diag
-
-        dist(i,1) = newVal;
-    end
-    
-    % process all pixels inside the current line
-    for j = 2:D2-1
-        % computes only for forground pixels
-        if ~img(i, j)
+for i = 1:D1
+    for j = 1:D2
+        % computes only for pixels within a region
+        if img(i, j) == 0
             continue;
         end
         
         % compute minimal propagated distance
         newVal = dist(i, j);
-        for k = 1:4
-            newVal = min(newVal, dist(i+di1(k), j+dj1(k))+ws(k));
+        for k = 1:nShifts
+            % coordinate of neighbor
+            i2 = i + di1(k);
+            j2 = j + dj1(k);
+            
+            % check bounds
+            if i2 < 1 || i2 > D1 || j2 < 1 || j2 > D2
+                continue;
+            end
+            
+            % compute new value
+            if img(i2, j2) == img(i, j)
+                % neighbor in same region 
+                % -> add offset weight to neighbor distance
+                newVal = min(newVal, dist(i2, j2) + ws(k));
+            else
+                % neighbor in another region 
+                % -> initialize with the offset weight
+                newVal = min(newVal, ws(k));
+            end
+            
         end
         
         % if distance was changed, update result
         dist(i,j) = newVal;
-    end
-    
-    % special processing for last pixel of the line
-    if img(i, D2)
-        newVal = dist(i, D2);
-        newVal = min(newVal, dist(i,   D2-1) + w1);  % left pixel
-        newVal = min(newVal, dist(i-1, D2-1) + w2);  % top-left pixel
-        newVal = min(newVal, dist(i-1, D2)   + w1);  % top pixel
-
-        dist(i,D2) = newVal;
     end
     
 end % iteration on lines
@@ -204,52 +215,47 @@ end % iteration on lines
 %% Backward iteration
 
 if verbose
-    disp(sprintf('Backward iteration %d', nIter)); %#ok<DSPS>
+    disp('Backward iteration');
 end
 
-% Process last image line
-for j = D2-1:-1:1
-    if img(D1, j)
-        dist(D1,j) = min(dist(D1,j), dist(D1, j+1)+w1);
-    end
-end
-
-for i = D1-1:-1:1
-    % special processing for last pixel of the line
-    if img(i, D2)
-        newVal = dist(i, D2);
-        newVal = min(newVal, dist(i+1,   D2) + w1); % bottom pixel
-        newVal = min(newVal, dist(i+1, D2-1) + w2); % bottom-left diag
-
-        dist(i,D2) = newVal;
-    end
-    
-    % process all pixels inside the current line
-    for j = D2-1:-1:2
+for i = D1:-1:1
+    for j = D2:-1:2
         % computes only for foreground pixels
-        if ~img(i, j)
+        if img(i, j) == 0
             continue;
         end
         
         % compute minimal propagated distance
         newVal = dist(i, j);
-        for k = 1:4
-            newVal = min(newVal, dist(i+di2(k), j+dj2(k))+ws(k));
+        for k = 1:nShifts
+            % coordinate of neighbor
+            i2 = i + di2(k);
+            j2 = j + dj2(k);
+            
+            % check bounds
+            if i2 < 1 || i2 > D1 || j2 < 1 || j2 > D2
+                continue;
+            end
+            
+            % compute new value
+            if img(i2, j2) == img(i, j)
+                % neighbor in same region 
+                % -> add offset weight to neighbor distance
+                newVal = min(newVal, dist(i2, j2) + ws(k));
+            else
+                % neighbor in another region 
+                % -> initialize with the offset weight
+                newVal = min(newVal, ws(k));
+            end
+             
         end
         
         % if distance was changed, update result
         dist(i,j) = newVal;
     end
     
-    % process last image line
-    if img(i, 1)
-        newVal = dist(i, 1);
-        newVal = min(newVal, dist(i,   2) + w1);  % right pixel
-        newVal = min(newVal, dist(i+1, 2) + w2);  % bottom-right pixel
-        newVal = min(newVal, dist(i+1, 1) + w1);  % bottom pixel
-
-        dist(i,1) = newVal;
-    end
-    
 end % line iteration
 
+if normalize
+    dist(dist>0) = dist(dist>0) / w1;
+end

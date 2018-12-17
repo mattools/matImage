d74ee2ec6d43e3381020c3ce7df117f8ff6e7551
dlegland <david.legland@inra.fr>
2018-12-17 10:16:54 +0100

    imFilters/imPrincipalAxesAlign.m: add support for axis specifications

--- matImage/imFilters/imPrincipalAxesAlign.m
+++ matImage/imFilters/imPrincipalAxesAlign.m
@@ -2,6 +2,8 @@ function res = imPrincipalAxesAlign(img, varargin)
 %IMPRINCIPALAXESALIGN Aligns image along principal axes of inertia matrix
 %
 %   RES = imPrincipalAxesAlign(IMG)
+%   RES = imPrincipalAxesAlign(DIM2, IMG)
+%   RES = imPrincipalAxesAlign(LX, LY, LZ, IMG)
 %
 %   (Note: currently only implemented for 3D images)
 %
@@ -19,14 +21,58 @@ function res = imPrincipalAxesAlign(img, varargin)
 % Created: 2018-12-17,    using Matlab 9.5.0.944444 (R2018b)
 % Copyright 2018 INRA - Cepia Software Platform.
 
+%% Default parameters
+
+% x-axis calibration
+% if empty -> lx ly an lz were not initialized
+lx = [];
+
 spacing = [1 1 1];
 
+% interpolation method
+method = 'linear';
+
+
+%% Parse inputs
+
+% check if first input contains axes basis
+if min(size(img)) == 1
+    if size(img, 2) == 3 && nargin >= 2
+        % the final size of the image is specified
+        lx = (1:img(1)) - img(1)/2;
+        ly = (1:img(2)) - img(2)/2;
+        lz = (1:img(3)) - img(3)/2;
+        img = varargin{1};
+        varargin(1) = [];
+        
+    elseif nargin >= 4
+        % the three first arguments contains lx, ly and lz
+        lx = img;
+        ly = varargin{1};
+        lz = varargin{2};
+        img = varargin{3};
+        varargin(1:3) = [];
+        
+    else
+        error('Unable to define size of resulting image');
+    end
+end
+
 dim = size(img);
+
+% check image data type
 binary = islogical(img);
 if ~binary
     error('Requires a binary image as input');
 end
-    
+
+% eventually parse interpolation method
+if ~isempty(varargin)
+    method = varargin{1};
+end
+
+
+%% Computation of Principal axes
 
 % extract points of the current particle
 inds = find(img > 0);
@@ -53,6 +99,9 @@ covPts = cov(points, 1) + diag(1/12 * ones(1, 3));
 % to extract inertia axes
 [U, S] = svd(covPts); %#ok<ASGLU>
 
+
+%% Format the transform matrix
+
 % create rotation matrix
 rot = [U zeros(3, 1); 0 0 0 1];
 
@@ -60,12 +109,21 @@ rot = [U zeros(3, 1); 0 0 0 1];
 tra = createTranslation3d(center);
 trans = tra * rot;
 
+
+%% Resample original image
+
+% create default grid parameters for resampling
+if isempty(lx)
+    lx = (1:dim(2)) - dim(2)/2;
+    ly = (1:dim(1)) - dim(1)/2;
+    lz = (1:dim(3)) - dim(3)/2;
+end
+
 % create sampling grid
-lx = -50:50;
-[x, y, z] = meshgrid(lx, lx, lx);
+[x, y, z] = meshgrid(lx, ly, lz);
 
 % transform the sampling grid to correspond to main axes of inertia matrix
 [x2, y2, z2] = transformPoint3d(x, y, z, trans);
 
 % create resulting image by interpolation
-res = imEvaluate(img, x2, y2, z2, 'linear') > 0.5;
+res = imEvaluate(img, x2, y2, z2, method) > 0.5;

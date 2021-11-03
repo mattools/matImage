038da8759525badc72c31bf84d8fee1bc77e877a
dlegland <david.legland@inrae.fr>
2021-11-03 10:14:45 +0100

    imMinkowski/imEuler3d.m: fix bug in management of 26 connectivity

--- tests/imMinkowski/test_imEuler3d.m
+++ tests/imMinkowski/test_imEuler3d.m
@@ -49,3 +49,115 @@ assertEqual(testCase, epcTh, imEuler3d(img));
 assertEqual(testCase, epcTh, imEuler3d(img, 6));
 assertEqual(testCase, epcTh, imEuler3d(img, 26));
 
+
+
+function test_3d_cube_C6(testCase)
+
+% create a simple cube
+img = false([5 5 5]);
+img(2:4, 2:4, 2:4) = true;
+
+% check EPC=1
+epcTh = 1;
+assertEqual(testCase, epcTh, imEuler3d(img, 6));
+
+% add a hole in the ball -> EPC=2
+img(3, 3, 3) = false;
+
+% check EPC=2
+epcTh  = 2;
+assertEqual(testCase, epcTh, imEuler3d(img, 6));
+
+
+function test_3d_cube_C26(testCase)
+
+% create a simple cube
+img = false([5 5 5]);
+img(2:4, 2:4, 2:4) = true;
+
+% check EPC=1
+epcTh = 1;
+assertEqual(testCase, epcTh, imEuler3d(img, 26));
+
+% add a hole in the ball -> EPC=2
+img(3, 3, 3) = false;
+
+% check EPC=2
+epcTh  = 2;
+assertEqual(testCase, epcTh, imEuler3d(img, 26));
+
+
+
+function test_3d_cubeRing_C6(testCase)
+
+% create a simple cubic ring
+img = false([5 5 5]);
+img(2:4, 2:4, 2:4) = true;
+img(3, 3, 2:4) = false;
+
+% check EPC=0
+epcTh = 0;
+assertEqual(testCase, epcTh, imEuler3d(img, 6));
+
+
+function test_3d_cubeRing_C26(testCase)
+
+% create a cubic ring
+img = false([5 5 5]);
+img(2:4, 2:4, 2:4) = true;
+img(3, 3, 2:4) = false;
+
+% check EPC=0
+epcTh = 0;
+assertEqual(testCase, epcTh, imEuler3d(img, 26));
+
+
+function test_3D_cubeDiagonals_C6(testCase)
+
+% create a small 3D image with points along cube diagonal
+img = false([5 5 5]);
+for i = 1:5
+    img(i, i, i) = true;
+    img(6-i, i, i) = true;
+    img(i, 6-i, i) = true;
+    img(6-i, 6-i, i) = true;
+end
+
+epc6 = imEuler3d(img, 6);
+
+epc6Th = 17;
+assertEqual(testCase, epc6, epc6Th);
+
+
+function test_3D_cubeDiagonals_C26(testCase)
+
+% create a small 3D image with points along cube diagonal
+img = false([7 7 7]);
+for i = 2:6
+    img(i, i, i) = true;
+    img(8-i, i, i) = true;
+    img(i, 8-i, i) = true;
+    img(8-i, 8-i, i) = true;
+end
+
+epc26 = imEuler3d(img, 26);
+
+epc26Th = 1;
+assertEqual(testCase, epc26, epc26Th);
+
+
+function test_3D_cubeDiagonals_touchingBorder_C26(testCase)
+
+% create a small 3D image with points along cube diagonal
+img = false([5 5 5]);
+for i = 1:5
+    img(i, i, i) = true;
+    img(6-i, i, i) = true;
+    img(i, 6-i, i) = true;
+    img(6-i, 6-i, i) = true;
+end
+
+epc26 = imEuler3d(img, 26);
+
+epc26Th = 1;
+assertEqual(testCase, epc26, epc26Th);

911916c691d2f48b2a96725dbb1c57a5c7a956ce
dlegland <david.legland@inrae.fr>
2023-05-10 16:23:00 +0200

    update installation script

--- matImage/installMatImage.m
+++ matImage/installMatImage.m
@@ -1,31 +1,30 @@
 function installMatImage(varargin)
-%INSTALLMATIMAGE Add the paths required for using MATIMAGE library
+%INSTALLMATIMAGE Add all the paths required for using the MATIMAGE library.
 %
 %   usage: 
 %       installMatImage;
 %   All the required directories are successively added, in appropriate
 %   order to comply with dependencies.
 %
-%   Example
-%   installMatImage
+%   Example:
+%     installMatImage
 %
 %   See also
-%   matImage
+%     matImage
 %
 
 % ------
 % Author: David Legland
-% e-mail: david.legland@grignon.inra.fr
+% e-mail: david.legland@inrae.fr
 % Created: 2008-01-17,    using Matlab 7.4.0.287 (R2007a)
 % Copyright 2008 INRA - BIA PV Nantes - MIAJ Jouy-en-Josas.
 % Licensed under the terms of the modified BSD licence
 
-% extract library path
-fileName = mfilename('fullpath');
-libDir = fileparts(fileName);
-
+% The list of "modules" of the library, that correspond to the
+% sub-directories from the main directory.
 moduleNames = {...
     'imExplore', ...
+    'imStacks', ...
     'imFilters', ...
     'imQuantify', ...
     'imMinkowski', ...
@@ -33,14 +32,20 @@ moduleNames = {...
     'imGranulometry', ...
     'imShapes'};
 
-disp('Installing MatImage Library');
+% extract library base path
+fileName = mfilename('fullpath');
+libDir = fileparts(fileName);
+
+% add the base path
+disp('Installing the MatImage Library');
 addpath(libDir);
 
-% add all library modules
+% add each library module
 for i = 1:length(moduleNames)
     name = moduleNames{i};
     fprintf('Adding module: %-20s', name);
     addpath(fullfile(libDir, name));
-    disp(' (done)');
+    fprintf(' (done)\n');
 end
 
+disp('Success: MatImage Library fully installed.');
